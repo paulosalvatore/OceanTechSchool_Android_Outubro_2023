509c3063246fae10fd57abe30740d2af11392472
Paulo Salvatore <salvatoregames@gmail.com>
2023-10-11 19:49:44 -0300

    Initial commit

---
+++ app/src/main/java/com/oceanbrasil/oceantechschool_android_outubro_2023/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oceanbrasil.oceantechschool_android_outubro_2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "OceanTechSchool_Android_Outubro_2023"
+include(":app")
