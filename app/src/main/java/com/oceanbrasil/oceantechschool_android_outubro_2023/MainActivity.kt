19b6e554cf57a43e664fc16b966e0db98a4bccb8
Paulo Salvatore <salvatoregames@gmail.com>
2023-10-11 20:35:42 -0300

    `PokeApiService` criado juntamente com entidade de retorno

--- app/src/main/java/com/oceanbrasil/oceantechschool_android_outubro_2023/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/oceantechschool_android_outubro_2023/MainActivity.kt
@@ -2,6 +2,29 @@ package com.oceanbrasil.oceantechschool_android_outubro_2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Call
+import retrofit2.http.GET
+
+data class ListPokemonResult(
+    val name: String,
+    val url: String
+)
+
+data class ListPokemonApiResult(
+    val count: Int,
+    val next: String?,
+    val previous: String?,
+    val results: Array<ListPokemonResult>
+)
+
+interface PokeApiService {
+
+    // https://pokeapi.co/api/v2/pokemon?limit=20&offset=0
+    // Base: https://pokeapi.co/api/v2/
+    // Endpoint (Rota): pokemon?limit=20&offset=0
+    @GET("pokemon?limit=20&offset=0")
+    fun listPokemon(): Call<ListPokemonApiResult>
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
