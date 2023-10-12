4e8794f9b5d4033f2a9b52b7c35d8b69a79622a1
Paulo Salvatore <salvatoregames@gmail.com>
2023-10-11 21:15:14 -0300

    Chamada para API implementada (exibindo apenas logs, por enquanto)

--- app/src/main/java/com/oceanbrasil/oceantechschool_android_outubro_2023/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/oceantechschool_android_outubro_2023/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.oceanbrasil.oceantechschool_android_outubro_2023
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+
 
 data class ListPokemonResult(
     val name: String,
@@ -27,8 +33,33 @@ interface PokeApiService {
 }
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(PokeApiService::class.java)
+
+        val call = service.listPokemon()
+
+        call.enqueue(object : Callback<ListPokemonApiResult> {
+            override fun onResponse(
+                call: Call<ListPokemonApiResult>,
+                response: Response<ListPokemonApiResult>
+            ) {
+                // Caso a requisição HTTP tenha sido bem sucedida
+                Log.d("POKEMON_API", response.body().toString())
+            }
+
+            override fun onFailure(call: Call<ListPokemonApiResult>, t: Throwable) {
+                // Caso a requisição HTTP tenha falhado
+                Log.e("POKEMON_API", "Erro ao carregar API.", t)
+            }
+        })
     }
 }
