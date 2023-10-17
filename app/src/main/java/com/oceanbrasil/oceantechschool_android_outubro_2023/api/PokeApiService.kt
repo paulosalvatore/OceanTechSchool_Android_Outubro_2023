497b678db1729170a0e46d08419581e736aa91ec
Paulo Salvatore <salvatoregames@gmail.com>
2023-10-16 22:26:09 -0300

    Carregando todos os Pokémon em vez de apenas 20

--- app/src/main/java/com/oceanbrasil/oceantechschool_android_outubro_2023/api/PokeApiService.kt
+++ app/src/main/java/com/oceanbrasil/oceantechschool_android_outubro_2023/api/PokeApiService.kt
@@ -8,6 +8,6 @@ interface PokeApiService {
     // https://pokeapi.co/api/v2/pokemon?limit=20&offset=0
     // Base: https://pokeapi.co/api/v2/
     // Endpoint (Rota): pokemon?limit=20&offset=0
-    @GET("pokemon?limit=20&offset=0")
+    @GET("pokemon?limit=10000&offset=0")
     fun listPokemon(): Call<ListPokemonApiResult>
 }
