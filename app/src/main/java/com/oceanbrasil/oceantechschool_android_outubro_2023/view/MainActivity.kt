346f92d53e34555caf6dd6bb8fd4d463399889fe
Paulo Salvatore <salvatoregames@gmail.com>
2023-10-18 20:50:14 -0300

    Imports da `MainActivity` otimizados

--- app/src/main/java/com/oceanbrasil/oceantechschool_android_outubro_2023/view/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/oceantechschool_android_outubro_2023/view/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.oceanbrasil.oceantechschool_android_outubro_2023.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.oceanbrasil.oceantechschool_android_outubro_2023.R
-import com.oceanbrasil.oceantechschool_android_outubro_2023.model.api.ListPokemonApiResult
-import com.oceanbrasil.oceantechschool_android_outubro_2023.model.api.PokeApiService
-import com.oceanbrasil.oceantechschool_android_outubro_2023.view.list.PokemonItem
 import com.oceanbrasil.oceantechschool_android_outubro_2023.view.list.PokemonListAdapter
 import com.oceanbrasil.oceantechschool_android_outubro_2023.viewmodel.PokemonViewModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
